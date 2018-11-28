69ae8c29844bf049cd9541df2e00be40b3a59c18
Harmin <cejba.patryk@gmail.com>
2018-11-28 16:13:53 +0100

    Repair second expect

--- pages/example-pages.ts
+++ pages/example-pages.ts
@@ -4,29 +4,31 @@ import { protractor } from 'protractor/built/ptor';
 
 export class HomePage{
     public url: any;
-    public enterText: ElementFinder;
-    public firstTodo: ElementFinder;
-    public clickToAdd: ElementFinder;
+    public textBox: ElementFinder;
+    public listTodo: ElementArrayFinder;
+    public secondTodo: ElementFinder;
+    public thirdTodo: ElementFinder;
+    public clickOnTextBox: ElementFinder;
     public markDoneElement: ElementFinder;
     public completedTab: ElementFinder;
     public activeTab: ElementFinder;
-    public remainingCountElement: ElementFinder;
-    public clearCompletedButton: ElementFinder;
-    public sharpSign: ElementFinder;
+    public remainingItem: ElementFinder;
+    public clearCompletedItem: ElementFinder;
+    public deletedItem: ElementFinder;
 
 
     constructor(){
         this.url = 'http://todomvc.com/examples/angularjs/#/';
 
-        this.enterText = $('[ng-model="newTodo"]');
-        this.firstTodo = $$('[class="ng-binding"]').get(0);
-        this.clickToAdd = $('[ng-model="newTodo"]');
+        this.textBox = $('[ng-model="newTodo"]');
+        this.listTodo = $$('.todo-list label');
+        this.clickOnTextBox = $('[ng-model="newTodo"]');
         this.markDoneElement = $('[ng-model="todo.completed"]');
         this.completedTab = $$('[class="footer"] [ng-class]').get(2);
         this.activeTab = $$('[class="footer"] [ng-class]').get(1);
-        this.remainingCountElement= $$('[class="footer"] [class="ng-binding"]').get(0);
-        this.clearCompletedButton= $('[ng-click="clearCompletedTodos()"]');
-        this.sharpSign= $('[ng-click="removeTodo(todo)"]').get(0);
+        this.remainingItem = $('[class="footer"] [class="ng-binding"]');
+        this.clearCompletedItem= $('[ng-click="clearCompletedTodos()"]');
+        this.deletedItem= $('[ng-click="removeTodo(todo)"]');
 
     }
 
@@ -34,18 +36,15 @@ export class HomePage{
         browser.get(this.url);
     }
 
-    public clickToadd(): void {
-        this.clickToAdd.click();
-    }
-
-
     public addTodo(addTodoElement){
-        this.enterText.sendKeys(addTodoElement);
-        this.clickToAdd.sendKeys(protractor.Key.ENTER);
+        this.clickOnTextBox.click();
+        this.textBox.sendKeys(addTodoElement);
+        this.clickOnTextBox.sendKeys(protractor.Key.ENTER);
     }
 
     public getTodo(): promise.Promise<string> {
-        return this.firstTodo.getText();
+        let item = this.listTodo.first()
+        return item.getText();
     }
 
     public markCompleted(): void{
@@ -60,17 +59,26 @@ export class HomePage{
         this.activeTab.click();
     }
 
-    public remainingCount(): promise.Promise<string>{
-        return this.remainingCountElement.getText();
+    public getTodoSecond(): promise.Promise<string> {
+        return this.secondTodo.getText();
     }
 
-    public clearCompletedTodos(): void{
-        this.clearCompletedButton.click();
+    public getTodoThird(): promise.Promise<string> {
+        return this.listTodo.getText();
+    }
+
+    public remainingCount(): promise.Promise<string> {
+        return this.remainingItem.getText();
+    }
+
+    public clearCompletedTodos(): void {
+        this.clearCompletedItem.click();
     }
 
     public deletedEelement(): void{
-        this.sharpSign.click();
+        this.deletedItem.click();
     }
 
-
 }
+
+
--- specs/example-spec.ts
+++ specs/example-spec.ts
@@ -10,49 +10,52 @@ describe('Testing todomvc.com with angularjs ', () => {
     beforeAll(() => {
     homePage.open()
     })
-
+    //1
     it('Should open the page',()=> {
         expect(browser.getCurrentUrl()).toBe(homePage.url);
     })
-
+    //2
     it('TODO list should contains one element "Make dinner"', ()=> {
-        homePage.clickToadd();
         homePage.addTodo('Make dinner');
         expect<any>(homePage.getTodo()).toEqual('Make dinner');
     });
-
+    //3
+    /*
     it('TODO list should contains one element, which is marked as done', ()=> {
         homePage.markCompleted();
         expect<any>(homePage.remainingCount()).toEqual('0');
     });
-
+    //4
     it('TODO list of active elements contains one element', ()=> {
         homePage.clickOnActiveTab();
         homePage.addTodo('Clear kitchen');
         expect<any>(homePage.getTodo()).toEqual('Clear kitchen');
     })
-
+    //5
     it('TODO list of completed elements contains one element', ()=> {
         homePage.clickOnCompletedTab();
         expect<any>(homePage.getTodo()).toEqual('Make dinner');
     })
-
+    //6
     it('Marked element will be removed from the list', ()=> {
-        homePage.clearCompletedTodos();
-        expect<any>(homePage.remainingCount()).toEqual('1');
-    })
-
-    it('Deleted element will be removed from the list', ()=> {
-        homePage.addTodo('Kiss');
-        homePage.clickOnActiveTab();
         homePage.deletedEelement();
         expect<any>(homePage.remainingCount()).toEqual('1');
     })
-
+    //7
+    it('Deleted element will be removed from the list', ()=> {
+        homePage.addTodo('Kiss');
+        homePage.clickOnActiveTab();
+        homePage.clearCompletedTodos();
+        expect<any>(homePage.remainingCount()).toEqual('1');
+    })
+    //8
     it('TODO list contains two elements of "Runing"', ()=> {
+        homePage.addTodo('Runing');
+        homePage.addTodo('Runing');
+        //expect<any>(homePage.secondTodo()).toEqual('Runing');
         
     })
-
+    //9
     it('Elemenet shouldnt be added to list', ()=> {
         
     })
@@ -67,7 +70,7 @@ describe('Testing todomvc.com with angularjs ', () => {
 
     it('TODO list contains elements.', ()=> {
         
-    })
+    })*/
 
 
 });
